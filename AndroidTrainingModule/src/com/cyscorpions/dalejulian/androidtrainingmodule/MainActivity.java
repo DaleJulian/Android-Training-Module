4ca324e49ba6e10c0ececb0732a545ce002541bd
dalejulian <dale@UNIT021.(none)>
2015-06-16 08:42:41 +0800

    Implemented expanded menu

--- AndroidTrainingModule/src/com/cyscorpions/dalejulian/androidtrainingmodule/MainActivity.java
+++ AndroidTrainingModule/src/com/cyscorpions/dalejulian/androidtrainingmodule/MainActivity.java
@@ -1,35 +1,59 @@
 package com.cyscorpions.dalejulian.androidtrainingmodule;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
+	
+	private CheckBox mCheckBox;
+	private TextView mTextView;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        mCheckBox = (CheckBox)findViewById(R.id.main_checkbox);
+        mTextView = (TextView)findViewById(R.id.main_textView);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+    public boolean onPrepareOptionsMenu(Menu menu){
+    	menu.setGroupVisible(1, mCheckBox.isChecked());
+    	return super.onPrepareOptionsMenu(menu);
     }
+    
+    @Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		menu.add(0, 1, 0, "add");
+		menu.add(0, 2, 0, "edit");
+		menu.add(0, 3, 3, "delete");
+		menu.add(1, 4, 1, "copy");
+		menu.add(1, 5, 2, "paste");
+		menu.add(1, 6, 4, "exit");
+
+		return super.onCreateOptionsMenu(menu);
+
+	}
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	public boolean onOptionsItemSelected(MenuItem item) {
+		StringBuilder sb = new StringBuilder();
+
+		// print the info about pressed menu item
+		sb.append("Item Menu");
+		sb.append("\r\n groupId: " + String.valueOf(item.getGroupId()));
+		sb.append("\r\n itemId: " + String.valueOf(item.getItemId()));
+		sb.append("\r\n order: " + String.valueOf(item.getOrder()));
+		sb.append("\r\n title: " + item.getTitle());
+		mTextView.setText(sb.toString());
+		return super.onOptionsItemSelected(item);
+	}
 }
